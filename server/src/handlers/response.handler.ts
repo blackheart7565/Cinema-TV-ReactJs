85e60bd35f2c5e004cd1c21c5e41268ab9c86d5f
blackheart7565 <give.me.heart8844@gmail.com>
2023-11-10 14:13:31 +0200

    Updated error handler

--- server/src/handlers/response.handler.ts
+++ server/src/handlers/response.handler.ts
@@ -1,39 +1,37 @@
 import { Response } from "express";
-
-interface IResponseHandlers { }
-interface IResponseData {
-	status: number;
-	message: string;
-}
+import { IResponseData, IResponseDataType, IResponseHandlers } from "../types/exceptions-types/exceptions";
 
 class ResponseHandlers implements IResponseHandlers {
 	constructor() { }
 
-	#responseWithData = (res: Response, statusCode: number, data: IResponseData): Response<any> =>
+	#responseWithData = (res: Response, statusCode: number, data: IResponseDataType): Response<any> =>
 		res.status(statusCode).json(data);
 
 	errors = (res: Response): Response<IResponseData> =>
 		this.#responseWithData(res, 500, { status: 500, message: "Oops! Something wrong, Internal Server Error" })
 
 	// Плохой запрос
-	badRequest = (res: Response, message: string): Response<IResponseData> =>
-		this.#responseWithData(res, 400, { status: 400, message })
+	badRequest = (res: Response, data: IResponseDataType): Response<IResponseData> =>
+		this.#responseWithData(res, 400, { status: 400, data })
 
 	// OK
-	ok = (res: Response, message: string): Response<IResponseData> =>
-		this.#responseWithData(res, 200, { status: 200, message })
+	ok = (res: Response, data: IResponseDataType): Response<IResponseData> =>
+		this.#responseWithData(res, 200, { status: 200, data })
 
-	// Создынный запрос
-	created = (res: Response, message: string): Response<IResponseData> =>
-		this.#responseWithData(res, 201, { status: 401, message })
+	/**
+	 * Создынный запрос
+	 *  The request has been fulfilled, resulting in the creation of a new resource.
+	 */
+	created = (res: Response, data: IResponseDataType): Response<any> =>
+		this.#responseWithData(res, 201, { status: 201, data })
 
 	// Неавторизованно
-	unauthorized = (res: Response): Response<IResponseData> =>
-		this.#responseWithData(res, 401, { status: 401, message: "Unauthorized" })
+	unauthorized = (res: Response, data: IResponseDataType): Response<IResponseData> =>
+		this.#responseWithData(res, 401, { status: 401, data: data || "Unauthorized" })
 
 	// Не найден
-	notFound = (res: Response, message: string): Response<IResponseData> =>
-		this.#responseWithData(res, 404, { status: 404, message: "Resource not found" })
+	notFound = (res: Response, data: IResponseDataType): Response<IResponseData> =>
+		this.#responseWithData(res, 404, { status: 404, data: data || "Resource not found" })
 }
 
 export default new ResponseHandlers();
