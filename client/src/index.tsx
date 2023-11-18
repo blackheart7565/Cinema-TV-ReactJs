d1380e82d1937f359553fc7d58c8cfbd9db3d79e
blackheart7565 <give.me.heart8844@gmail.com>
2023-11-18 22:07:47 +0200

    The main content skeleton has been created

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
+import AppRouters from "./component/model/AppRouters";
+
+import "./styles/base.scss";
+import "./styles/index.scss";
+import "./styles/reset.scss";
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
+
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<AppRouters />
+		</BrowserRouter>
 	</React.StrictMode>
 );
