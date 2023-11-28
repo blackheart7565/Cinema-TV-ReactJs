af2c9dd47a96003e0f689211442bfc55070214e8
blackheart7565 <give.me.heart8844@gmail.com>
2023-11-28 18:07:53 +0200

    Created configuration to movie and serials.

---
+++ client/src/api/config/media.config.ts
@@ -0,0 +1,20 @@
+
+export const mediaConfig = {
+	category: {
+		// Get a list of movies ordered by popularity.
+		popular: "popular",
+
+		// Get a list of movies ordered by rating.
+		top_rated: "top_rated",
+
+		// Get a list of movies that are currently in theatres.
+		now_playing_movie: "now_playing",
+
+		// Get a list of TV shows airing today.
+		airing_today_tv: "airing_today",
+	},
+	types: {
+		movie: "movie",
+		tv: "tv",
+	}
+}
