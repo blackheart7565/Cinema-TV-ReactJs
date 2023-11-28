c61b538585ea23bdbb57d36bd3abcb9bda51297a
blackheart7565 <give.me.heart8844@gmail.com>
2023-11-28 18:11:13 +0200

    Created endpoints movie and serials.
    Created method for getting a list of movies and TV series.

---
+++ client/src/api/modules/media.api.ts
@@ -0,0 +1,52 @@
+import {
+	IMediaDetails,
+	IMediaGenre,
+	IMediaList,
+	IMediaSearch
+} from "../../types/media.types";
+import publicAxios from "../client/public.client";
+
+const mediaEndpoints = {
+	list: ({ mediaType, mediaCategory, page }: IMediaList): string =>
+		`/${mediaType}/list/${mediaCategory}?page=${page}`,
+
+	details: ({ mediaType, mediaId }: IMediaDetails): string =>
+		`/${mediaType}/details/${mediaId}`,
+
+	genre: ({ mediaType }: IMediaGenre): string =>
+		`/${mediaType}/genre`,
+
+	search: ({ mediaType, query, page }: IMediaSearch): string =>
+		`/${mediaType}/search?query=${query}$page=${page}`,
+}
+
+class MediaApi {
+	async getList({ mediaType, mediaCategory, page }: IMediaList): Promise<any> {
+		try {
+			const response = await publicAxios.get(
+				mediaEndpoints.list({ mediaType, mediaCategory, page })
+			);
+			return response;
+		} catch (error) {
+			return { error }
+		}
+	}
+
+	async getDetails(mediaType: string, mediaId: string): Promise<any> {
+		try {
+
+		} catch (error) {
+			return { error }
+		}
+	}
+
+	async search(mediaType: string, query: string, page: string): Promise<any> {
+		try {
+
+		} catch (error) {
+			return { error }
+		}
+	}
+}
+
+export default new MediaApi();
