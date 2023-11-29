3609f81ab8db6103c9f3b44ff5372419de580993
blackheart7565 <give.me.heart8844@gmail.com>
2023-11-29 20:36:56 +0200

    Added to media configs, methods for concatenating post images.

--- client/src/component/model/Layout.tsx
+++ client/src/component/model/Layout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { useReducer } from "../../hooks/reducer.hook";
 import Footer from "./Footer";
 import GlobalLoader from "./GlobalLoader";
 import Header from "./Header";
 import Main from "./Main";
 
 const Layout = () => {
-	const isLoad = false;
+	const { state } = useReducer();
 
 	return (
 		<div className="wrapper">
