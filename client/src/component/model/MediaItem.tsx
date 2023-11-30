5558ae605012bac89cd7bc21797b3b10f18bc6be
blackheart7565 <give.me.heart8844@gmail.com>
2023-11-30 23:59:09 +0200

    Created star rating.

--- client/src/component/model/MediaItem.tsx
+++ client/src/component/model/MediaItem.tsx
@@ -2,6 +2,9 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 
 import { mediaConfig } from "../../api/config/media.config";
+import RatingStar from "./RatingStar";
+
+// type IRating = string | number;
 
 interface IMediaItem {
 	posterImage: string | undefined;
@@ -10,6 +13,22 @@ interface IMediaItem {
 	rating?: string | number;
 	season?: string | number;
 	episode?: string | number;
+}
+
+const convertorDouble = (rating: string | number): number => {
+	return parseInt(rating.toString().replace(/(\d+)(\.|,)(\d+)/g, function (o: any, a: any, b: any, c: any) {
+		return a + b + c.slice(0, 1);
+	}));
+}
+
+const ConvertorRating = (rating: string | number) => {
+	if (!rating) return;
+
+	rating = rating.toString().replace(/(\d+)(\.|,)(\d+)/g, function (o: any, a: any, b: any, c: any) {
+		return a + b + c.slice(0, 1);
+	});
+
+	return rating
 }
 
 const MediaItem: FC<IMediaItem> = ({
@@ -35,7 +54,10 @@ const MediaItem: FC<IMediaItem> = ({
 					<img className="films__link-play-icon" src="/path/films/play-icon.png" alt="play-icon" />
 				</div>
 				<div className="films__link-details">
-					<div className="films__link-rating">{rating}</div>
+					<div className="films__link-rating">
+						<RatingStar rating={rating} />
+						{rating && ConvertorRating(rating)}
+					</div>
 					<p className="films__link-title">{name}</p>
 					<p className="films__link-year">{year}</p>
 				</div>
