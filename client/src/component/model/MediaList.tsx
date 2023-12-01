5a79ce1812823152ee6f269a899a3e1b8ebc8ab3
blackheart7565 <give.me.heart8844@gmail.com>
2023-12-01 21:35:44 +0200

    Updating the MediaList component.
    The business logic has been moved to the parent component.

--- client/src/component/model/MediaList.tsx
+++ client/src/component/model/MediaList.tsx
@@ -1,53 +1,15 @@
-import { FC, useEffect, useState } from "react";
-
-import mediaApi from "../../api/modules/media.api";
-import { useReducer } from "../../hooks/reducer.hook";
-import { IResponseMediasListResult } from "../../types/media.types";
-import MediaItem from "./MediaItem";
+import { FC, ReactNode } from "react";
 
 interface IMediaList {
-	mediaType: string;
-	mediaCategory: string;
-	page?: string | number;
+	children: ReactNode;
 }
 
 const MediaList: FC<IMediaList> = ({
-	mediaType,
-	mediaCategory,
-	page,
+	children
 }) => {
-	const [films, setFilms] = useState<IResponseMediasListResult[]>([]);
-	const { dispatch, actions } = useReducer();
-
-	const fetchFilms = async () => {
-		dispatch(actions.setIsLoading(true));
-		const response = await mediaApi.getList({
-			mediaType,
-			mediaCategory,
-			page: page || "1"
-		})
-		dispatch(actions.setIsLoading(false));
-
-		console.log(response.results);
-
-		setFilms(response.results);
-	}
-
-	useEffect(() => {
-		fetchFilms();
-	}, [mediaType, mediaCategory, dispatch]);
-
 	return (
 		<ul className="films__list">
-			{films.map(item => (
-				<MediaItem
-					key={item.id}
-					posterImage={item.poster_path || item.backdrop_path}
-					name={item.title || item.original_title}
-					year={item.release_date.split("-")[0]}
-					rating={item.vote_average}
-				/>
-			))}
+			{children}
 		</ul>
 	);
 }
--- client/src/component/page/Films/index.tsx
+++ client/src/component/page/Films/index.tsx
@@ -1,19 +1,67 @@
-import { FC } from "react";
-
+import { FC, useEffect, useState } from "react";
 
 import { mediaConfig } from "../../../api/config/media.config";
+import mediaApi from "../../../api/modules/media.api";
+import { useReducer } from "../../../hooks/reducer.hook";
+import { IResponseMediasListResultMovie } from "../../../types/media.types";
+import MediaItem from "../../model/MediaItem";
 import MediaList from "../../model/MediaList";
 import "./Films.scss";
 
 interface IFilms { }
 
 const Films: FC<IFilms> = () => {
+	const [films, setFilms] = useState<IResponseMediasListResultMovie[]>([]);
+	const [page, setPage] = useState<number>(1);
+	const [isMediaLoading, setIsMediaLoading] = useState<boolean>(false);
+
+	const { dispatch, actions } = useReducer();
+
+	const mediaType = mediaConfig.types.movie;
+	const mediaCategory = mediaConfig.category.popular;
+
+	const fetchFilms = async () => {
+		if (page === 1) dispatch(actions.setIsLoading(true));
+
+		const response = await mediaApi.getList({
+			mediaType,
+			mediaCategory,
+			page: page
+		})
+
+		dispatch(actions.setIsLoading(false));
+
+		setFilms(prev => [...prev, ...response.results]);
+	}
+
+	useEffect(() => {
+		fetchFilms();
+	}, [mediaType, mediaCategory, page, dispatch]);
+
 	return (
 		<div className="films">
-			<MediaList
-				mediaType={mediaConfig.types.movie}
-				mediaCategory={mediaConfig.category.popular}
-			/>
+			<MediaList>
+				{films.map(item => (
+					<MediaItem
+						key={item.id}
+						posterImage={item.poster_path || item.backdrop_path}
+						name={item.title || item.original_title}
+						year={item.release_date.split("-")[0]}
+						rating={item.vote_average}
+					/>
+				))}
+			</MediaList>
+
+			<button
+				style={{
+					width: "100px",
+					height: "50px",
+					textAlign: "center",
+				}}
+				onClick={(e) => setPage(prev => prev + 1)}
+			>
+				NEXT
+			</button>
 		</div>
 	);
 };
