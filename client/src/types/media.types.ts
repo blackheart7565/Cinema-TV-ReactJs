045fbdbe8ef63f95e2d5a027ae3db0dddb3cf5f2
blackheart7565 <give.me.heart8844@gmail.com>
2023-12-01 21:38:16 +0200

    Rozbeat the result of movie and tv serials types

--- client/src/types/media.types.ts
+++ client/src/types/media.types.ts
@@ -25,6 +25,23 @@ export interface IResponseMediasList<T extends IResponseMediasListResult> {
 	total_results: number
 }
 
+// export interface IResponseMediasListResult {
+// 	adult: boolean
+// 	backdrop_path?: string
+// 	genre_ids: number[]
+// 	id: number
+// 	original_language: string
+// 	original_title: string
+// 	overview: string
+// 	popularity: number
+// 	poster_path: string
+// 	release_date: string
+// 	title: string
+// 	video: boolean
+// 	vote_average: number
+// 	vote_count: number
+// }
+
 export interface IResponseMediasListResult {
 	adult: boolean;
 	backdrop_path?: string;
