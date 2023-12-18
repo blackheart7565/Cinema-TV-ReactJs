ae41547b559c7e1788599159866803582ea2a7c7
blackheart7565 <give.me.heart8844@gmail.com>
2023-12-18 16:16:04 +0200

    Removing an unused package.

--- client/src/component/Details/MediaDetailsHeader/MediaDetailsHeader.tsx
+++ client/src/component/Details/MediaDetailsHeader/MediaDetailsHeader.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import styled from "styled-components";
 
 interface IMediaDetailsHeaderProps {
 	src: string;
@@ -9,8 +8,7 @@ const MediaDetailsHeader: FC<IMediaDetailsHeaderProps> = ({
 	src
 }) => {
 	return (
-		// <div className="media-details__header"
-		<Header
+		<div className="media-details__header"
 			style={{
 				backgroundImage: `url(${src})`
 			}} />
@@ -18,29 +16,3 @@ const MediaDetailsHeader: FC<IMediaDetailsHeaderProps> = ({
 }
 
 export default MediaDetailsHeader;
-
-const Header = styled.div`
-	width: 100%;
-	height: 80vh;
-	position: relative;
-	background-attachment: fixed;
-	background-position: center top;
-	background-size: cover;
-	transition: all 0.3s ease-in-out;
-
-	@media screen and (max-width: 960px) {
-		height: 70vh;
-	}
-
-	&::before {
-		content: "";
-		width: 100%;
-		height: 101%;
-		position: absolute;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		background: linear-gradient(to top, #01000c, #01000c00);
-	}
-`;
