cfd07d65d437c2cf19d87579b0236348101e81fe
blackheart7565 <give.me.heart8844@gmail.com>
2023-12-19 20:38:26 +0200

    Removed a block of code in the MediaDetailsPanel.tsx file.

--- client/src/component/Details/MediaDetailsPanel/MediaDetailsPanel.tsx
+++ client/src/component/Details/MediaDetailsPanel/MediaDetailsPanel.tsx
@@ -63,23 +63,6 @@ const MediaDetailsPanel: FC<IMediaDetailsPanelProps> = ({
 												: null
 								)}
 								title={"Duration"} />
-							{
-								directors && directors?.length > 0 && (
-									<MediaDetailsInfoRow
-										className={"media-details__director"}
-										value={directors}
-										title={"Director"} />
-								)
-							}
-
-							{
-								countries && countries.length > 0 && (
-									<MediaDetailsInfoRow
-										className={"media-details__countries"}
-										value={countries}
-										title={"Country"} />
-								)
-							}
 
 							{directors && directors?.length > 0 && (
 								<MediaDetailsInfoRow
@@ -105,7 +88,7 @@ const MediaDetailsPanel: FC<IMediaDetailsPanelProps> = ({
 								value={description}
 								title={"Description"} />
 
-						</div >
+						</div>
 						<div className="media-details__content-right">
 							<div className="media-details__rating-container">
 								<div className="media-details__rating">
@@ -133,10 +116,11 @@ const MediaDetailsPanel: FC<IMediaDetailsPanelProps> = ({
 								className={"media-details__released"}
 								value={released}
 								title={"Released"} />
-						</div >
-					</div >
-				</section >
-			</div >
+						</div>
+					</div>
+
+				</section>
+			</div>
 		</>
 	)
 }
