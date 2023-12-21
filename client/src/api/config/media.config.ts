ea056459ca2b2b9976bc5abb30ad78283c4edb22
blackheart7565 <give.me.heart8844@gmail.com>
2023-12-21 15:59:08 +0200

    Added replace type methods in media config.

--- client/src/api/config/media.config.ts
+++ client/src/api/config/media.config.ts
@@ -18,8 +18,28 @@ export const mediaConfig = {
 	types: {
 		movie: "movie",
 		tv: "tv",
+		films: "films",
+		serials: "serials",
 	},
 	methods: {
+		replaceMovieAndTv_To_FilmsAndSerials: (mediaType: string): string => {
+			if (mediaType === "movie") {
+				return "films";
+			} else if (mediaType === "tv") {
+				return "serials";
+			} else {
+				return "";
+			}
+		},
+		replaceFilmsAndSerials_To_MovieAndTv: (mediaType: string): string => {
+			if (mediaType === "films") {
+				return "movie";
+			} else if (mediaType === "serials") {
+				return "tv";
+			} else {
+				return "";
+			}
+		},
 		poster_path: (imgEndpoint: string | undefined): string => `https://image.tmdb.org/t/p/original${imgEndpoint}`,
 		backdrop_path: (imgEndpoint: string | undefined): string => `https://image.tmdb.org/t/p/w5${imgEndpoint}`,
 		youtube_path: (imgEndpoint: string | undefined): string => `https://www.youtube.com/embed/${imgEndpoint}`,
