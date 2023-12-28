36abb338cfa19cfa0b17e7f31f018f45ad8a843e
blackheart7565 <give.me.heart8844@gmail.com>
2023-12-28 18:55:47 +0200

    Created custom input validation.

--- client/src/component/UI/Inputs/Input.tsx
+++ client/src/component/UI/Inputs/Input.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import classNames from "classnames";
+import React, { HTMLInputTypeAttribute } from 'react';
+import {
+	FieldErrors,
+	FieldValues,
+	UseFormRegister
+} from "react-hook-form";
 
-const Input = () => {
+import "./Input.scss";
+
+interface IInputProps {
+	wrapperClass?: string | undefined;
+	id: string;
+	label: string;
+	type?: HTMLInputTypeAttribute | undefined;
+	placeholder?: string | undefined;
+	required?: boolean;
+	disabled?: boolean | undefined;
+	register: UseFormRegister<FieldValues>;
+	errors: FieldErrors;
+}
+
+const Input: React.FC<IInputProps> = ({
+	wrapperClass,
+	id,
+	label,
+	type,
+	placeholder,
+	required,
+	disabled,
+	register,
+	errors,
+}) => {
 	return (
-		<div>
-			
+		<div className={classNames("Input_Box", wrapperClass)}>
+			<label htmlFor={id} className="Input_Box__label">
+				{label}
+			</label>
+			<div className="Input_Box__wrapper">
+				<input
+					className="Input_Box__enter"
+					type={type}
+					id={id}
+					autoComplete={id}
+					placeholder={placeholder}
+					disabled={disabled}
+					{...register(id, { required })}
+				/>
+			</div>
 		</div>
 	);
 };
