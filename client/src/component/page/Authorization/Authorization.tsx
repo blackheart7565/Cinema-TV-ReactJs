1739f28b0fa33d8edb3969b817cbe95525fb084a
blackheart7565 <give.me.heart8844@gmail.com>
2023-12-28 20:43:59 +0200

    Created page Authorizations.

--- client/src/component/page/Authorization/Authorization.tsx
+++ client/src/component/page/Authorization/Authorization.tsx
@@ -1,8 +1,13 @@
 import { useCallback, useState } from "react";
-import { Link } from "react-router-dom";
+import {
+	FieldValues,
+	SubmitHandler,
+	useForm
+} from "react-hook-form";
 
-import Logo from "../../model/Logo/Logo";
+import Input from "../../UI/Inputs/Input";
 
+import AuthButton from "../../UI/Button/AuthButton/AuthButton";
 import "./Authorization.scss";
 
 type IVariantType = "LOGIN" | "REGISTRATION";
@@ -12,6 +17,20 @@ const Authorization = () => {
 	const [variant, setVariant] = useState<IVariantType>("LOGIN");
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [title, setTitle] = useState<ITitleType>("Sing In");
+
+	const {
+		register,
+		handleSubmit,
+		formState: {
+			errors,
+		}
+	} = useForm<FieldValues>({
+		defaultValues: {
+			username: "",
+			email: "",
+			password: "",
+		}
+	});
 
 	const toggleVariant = useCallback(() => {
 		const timeout = 100;
@@ -28,22 +47,65 @@ const Authorization = () => {
 		}
 	}, [variant]);
 
+	const onSubmit: SubmitHandler<FieldValues> = (data: FieldValues) => {
+
+	};
+
 	return (
 		<div className="authorization">
+			<div className="authorization__bg">
+				<img className="authorization__img" src="/bg.jpg" alt="bg" />
+			</div>
 			<div className="authorization__wrapper">
-				<section className="authorization__header">
-					<Logo wrapperClassName="authorization__logo" />
-					<h2 className="authorization__title">{title}</h2>
-				</section>
-				<div className="authorization__content">
-					<Link
-						to={""}
-						className="authorization__link"
+				<h1 className="authorization__title">{title}</h1>
+				<form
+					className="authorization__form"
+					onSubmit={handleSubmit(onSubmit)}
+				>
+					{variant === "REGISTRATION" && (
+						<Input
+							wrapperClass="authorization__username"
+							id={"username"}
+							label="Username:"
+							type="text"
+							placeholder="Username"
+							register={register}
+							errors={errors}
+						/>
+					)}
+					<Input
+						wrapperClass="authorization__username"
+						id={"email"}
+						label="Email:"
+						type="email"
+						placeholder="Email"
+						register={register}
+						errors={errors}
+					/>
+					<Input
+						wrapperClass="authorization__username"
+						id={"password"}
+						label="Password:"
+						type="password"
+						placeholder="Password"
+						register={register}
+						errors={errors}
+					/>
+					<AuthButton
+						wrapperClass="authorization__auth-btn"
+						fullWidth
+						disabled={isLoading}
+						type="submit"
 						onClick={toggleVariant}
 					>
-						{title}
-					</Link>
-				</div>
+						{variant === "LOGIN" ? "Sign in" : "Registration"}
+					</AuthButton>
+
+					<div className="authorization__underline">
+						<div className="authorization__underline-inner">Or</div>
+					</div>
+
+				</form>
 			</div>
 		</div>
 	);
