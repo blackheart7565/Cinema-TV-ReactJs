21468dad1acb33bebf9d5069d46e386117ec9383
blackheart7565 <give.me.heart8844@gmail.com>
2024-01-13 20:08:56 +0200

    A component has been created for selecting a profile avatar.

---
+++ client/src/component/model/SelectAvatar/SelectAvatar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import "./SelectAvatar.scss";
+
+interface ISelectAvatarProps {
+
+}
+
+const SelectAvatar: React.FC<ISelectAvatarProps> = ({
+
+}) => {
+	return (
+		<div>
+
+		</div>
+	);
+};
+
+export default SelectAvatar;
